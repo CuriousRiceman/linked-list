e9c23439e8210a10dc069f241d0c2009f269a769
Richman <richmanong4@gmail.com>
2024-06-01 23:42:53 -1000

    Finished

---
+++ linkNode.js
@@ -0,0 +1,13 @@
+class LinkNode {
+    constructor(value, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+    // getNextNode() {
+    //     return this.nextNode;
+    // }
+    // setNextNode(nodeValue) {
+    //     this.nextNode = nodeValue;
+    // }
+}
+export default LinkNode;
---
+++ linkedList.js
@@ -0,0 +1,129 @@
+// import export is ES6, need to specify type module in package.json or rename extensions to .mjs
+// const LinkNode = require('./linkNode.js'); // CommonJS is default module system in Nodejs
+import LinkNode from './linkNode.js';
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+    prepend(value) {
+        if (this.tail === null && this.head === null) {
+            let data = new LinkNode(value);
+            this.tail = data;
+            this.head = data;
+        } else {
+            let data = new LinkNode(value);
+            data.nextNode = this.head;
+            this.head = data;
+        }
+    }
+    append(value) {
+        if (this.head === null && this.tail === null) {
+            let data = new LinkNode(value);
+            this.head = data;
+            this.tail = data;
+        } else {
+            let data = new LinkNode(value);
+            this.tail.nextNode = data;
+            this.tail = data;
+        }
+    }
+    getSize() {
+        let length = 0;
+        let tempHead = this.head;
+        while (tempHead !== null) {
+            length++;
+            tempHead = tempHead.nextNode;
+        }
+        console.log("Size of linked list is " + length);
+    }
+    getHead() {
+        return "Head points to " + this.head.value;
+    }
+    getTail() {
+        return "Tail points to " + this.tail.value;
+    }
+    at(index) {
+        let tempHead = this.head;
+        let instance = 0;
+        while(tempHead !== null && instance < index) {
+            tempHead = tempHead.nextNode;
+            instance++;
+        }
+        if(tempHead === null) {
+            return "Index does not exist";
+        } else {
+            return "Node at index " + index + " is " + tempHead.value;
+        }
+    }
+    pop() {
+        let tempHead = this.head;
+        while(tempHead.nextNode !== this.tail) {
+            tempHead = tempHead.nextNode;
+        }
+        tempHead.nextNode = null;
+        this.tail = tempHead;
+    }
+    contains(value) {
+        let tempHead = this.head;
+        let found = false;
+        while (tempHead !== null) {
+            if(value === tempHead.value) {
+                found = true;
+                console.log("List contains " + value);
+                break;
+            }
+            tempHead = tempHead.nextNode;
+        }
+        if(!found) {
+            console.log("List does NOT contain " + value);
+        }
+        
+    }
+    find(value) {
+        let tempHead = this.head;
+        let found = false;
+        let index = 0;
+        while(tempHead !== null) {
+            if(tempHead.value === value) {
+                found = true;
+                break;
+            }
+            index++;
+            tempHead = tempHead.nextNode;
+        }
+        if(!found) {
+            return "Index of " + value + " is " + null;
+        } else {
+            return "Index of " + value + " is " + index;
+        }
+
+    }
+    toString() {
+        let tempHead = this.head;
+        while (tempHead !== null) {
+            process.stdout.write("( " + tempHead.value + " ) -> "); // process.stdout.write will make it print on same line
+            tempHead = tempHead.nextNode;
+        }
+        process.stdout.write("" + tempHead);
+    }
+}
+const linkedList = new LinkedList;
+linkedList.prepend(5);
+linkedList.prepend(10);
+linkedList.append(6);
+linkedList.append(15);
+
+linkedList.getSize();
+linkedList.toString();
+console.log("\n------------------------------------------");
+linkedList.pop();
+linkedList.toString();
+
+console.log();
+console.log(linkedList.getHead());
+console.log(linkedList.getTail());
+console.log(linkedList.at(2));
+linkedList.contains(5);
+console.log(linkedList.find(6));
+
